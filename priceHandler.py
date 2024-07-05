f4d2ea8cfb620c71605681ed796538849ec28a1c
braydenmsue <braydenmsue@gmail.com>
2024-07-05 16:37:28 -0700

    added XPath for searching list items

--- priceHandler.py
+++ priceHandler.py
@@ -2,6 +2,7 @@ import listHandler as lh
 from selenium import webdriver
 import chromedriver_autoinstaller
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
@@ -20,18 +21,28 @@ def get_driver(self):
 
 class PriceFinder:
     def __init__(self, filename: str):
-        self.driver = get_driver()
         self.slh = lh.ShoppingListHandler()
         self.list = self.slh.get_list(filename)
         self.items = {}
 
     def gather_items_data(self):
+        driver = get_driver()
         search_bar_xpath = '//input[@id="search-bar-input"]'
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, search_bar_xpath)))
 
         for index, row in self.list.data.iterrows():
-            # self.items[row['name']] = {}
-            pass
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, search_bar_xpath)))
+            search_bar = driver.find_element(By.XPATH, search_bar_xpath)
+
+            search_bar.clear()
+            search_bar.send_keys(row['name'])
+            search_bar.send_keys(Keys.RETURN)
+
+            # TODO: determine blocks of page to scrape (under 'Results for "____"' header)
+
+
+
+
 
     def close_driver(self):
         self.driver.quit()
+
