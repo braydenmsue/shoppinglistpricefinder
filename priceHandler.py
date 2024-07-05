c9c13c8b1e2002a4115f4218bfa813ca1e550e43
braydenmsue <braydenmsue@gmail.com>
2024-07-05 16:51:18 -0700

    fixed search bar not clearing; used chrome_options to keep window open

--- priceHandler.py
+++ priceHandler.py
@@ -6,14 +6,21 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
+import time
+
+from selenium.webdriver.chrome.options import Options   # for testing
 
 WEBSITE = 'https://sameday.costco.ca/store/costco-canada/storefront'
 
 
-def get_driver(self):
+def get_driver():
     chromedriver_autoinstaller.install()
 
-    driver = webdriver.Chrome()
+    # for testing; keeps window open after script finishes
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get(WEBSITE)
     driver.maximize_window()
     return driver
@@ -33,16 +40,10 @@ class PriceFinder:
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, search_bar_xpath)))
             search_bar = driver.find_element(By.XPATH, search_bar_xpath)
 
-            search_bar.clear()
+            search_bar.send_keys(Keys.CONTROL + "a")
+            search_bar.send_keys(Keys.DELETE)
+
             search_bar.send_keys(row['name'])
             search_bar.send_keys(Keys.RETURN)
 
             # TODO: determine blocks of page to scrape (under 'Results for "____"' header)
-
-
-
-
-
-    def close_driver(self):
-        self.driver.quit()
-
